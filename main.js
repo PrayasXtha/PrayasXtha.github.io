dc8bbe5eaddf7c035ec0d968edcb15c44d0c15fe
Prayas <prayasshrestha127@gmail.com>
2022-06-28 17:04:09 +0545

    final commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// if else class
+// var age = 9
+// if (age >=10){
+//     console.log("you can drink rasna")
+// }
+// else{
+//     console.log("you can't drink rasna")
+// }
+
+// var a = [1,2,3,4,5,6,7];
+// b= 0;
+// while (b<a.length){
+//     b++;
+//     console.log(b);
+    
+// }
+
+// let myDate = new Date();
+//  console.log(myDate)
+
+// let a = 0;
+// while (a<=10){
+//     a += 2;
+//     console.log("senpai");
+    
+// }
+
